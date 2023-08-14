55c4101c841643bce7d26e518409329cbacca041
Apple <apple@Apples-MacBook-Air.local>
2023-08-14 15:00:18 +0530

    Initial commit with basic project structure

---
+++ test.py
@@ -0,0 +1,14 @@
+# TODO Input CCode dir, translation dir
+# TODO run translator.py
+# TODO run main.py in translated code
+# TODO Input input.txt to the process
+# TODO save results to test/test_no/result.txt file
+# TODO Compare result.txt and output.txt
+# TODO Return true if working 
+
+"""
+Check how to use pytest to make some of these processes simpler,
+probably not of much use in IT, but we can use it in DT, for 
+individual pieces. For individual functions.
+"""
+
